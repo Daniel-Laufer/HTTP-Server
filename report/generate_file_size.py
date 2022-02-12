777845455ba428e103335e0b21124f47b16c39d6
Daniel Laufer <lauferkdaniel@gmail.com>
2022-02-11 22:43:41 -0500

    cleaned up repo

--- report/generate_file_size.py
+++ report/generate_file_size.py
@@ -1,7 +1,6 @@
 mil = 1000000
-file_name = "2MB"
 n = 2 * mil
-for i in range(8,11):
+for i in range(1,10):
     n = (2**i) * mil
     with open(f"358_files/{n//mil}MB.txt", 'w') as f:
         for i in range(n):
